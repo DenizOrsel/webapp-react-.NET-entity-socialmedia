a643b9e3080ec8d669a0fded7adf0a355957ca34
Deniz Orsel <denizonerorsel@gmail.com>
2022-08-31 23:21:18 +0200

    Initialize layers

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+    
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await _context.Activities.ToListAsync();
+    }
+    [HttpGet("{id}")] //activities/id
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+        var activity = await _context.Activities.FindAsync(id);
+        if (activity == null)
+        {
+            return NotFound();
+        }
+        return activity;
+    }   
+    }
+}
